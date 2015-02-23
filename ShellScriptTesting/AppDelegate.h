0d6016cff3f12980396262ce125e01c29e4be0f7
Chris Bernt <christopher.bernt@duke.edu>
2015-02-23 10:24:26 -0500

    Initial Commit

---
+++ ShellScriptTesting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShellScriptTesting
+//
+//  Created by Chris on 2/23/15.
+//  Copyright (c) 2015 Chris Bernt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ShellScriptTesting/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ShellScriptTesting
+//
+//  Created by Chris on 2/23/15.
+//  Copyright (c) 2015 Chris Bernt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
